2acaca2fb0b2cd16e57f02e1512f47c9b729ceed
BoiseState Student <student@u.boisestate.edu>
2022-01-16 17:10:39 -0700

    Updated visualization panel

--- Mandelbrot/VisualizationPanel.java
+++ Mandelbrot/VisualizationPanel.java
@@ -1,6 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * This panel serves as a surface to visualize our data using the
+ *   2d graphics package. Not an optimal solution, but it serves to
+ *   demonstrate 2d arrays in CS121. :)
+ * @author Luke Hindman
+ */
 @SuppressWarnings("serial")
 public class VisualizationPanel extends JPanel {
 
@@ -23,7 +29,14 @@ public class VisualizationPanel extends JPanel {
         for(int row = 0; row < this.dataset.length; row++) {
             for (int col = 0; col < this.dataset[row].length; col++) {
                 int locationValue = this.dataset[row][col];
-                Color locationColor = this.palette[locationValue];
+                
+                Color locationColor;
+                if (locationValue < this.palette.length) {
+                    locationColor = this.palette[locationValue];
+                } else {
+                    locationColor = this.palette[palette.length-1];
+                }
+                
                 Rectangle current = new Rectangle(col,row,1,1);
                 pen.setColor(locationColor);
                 pen.fill(current);
@@ -33,13 +46,13 @@ public class VisualizationPanel extends JPanel {
 
 
     private Color[] buildGrayScalePalette() {
-        Color[] palette = new Color[1024];
+        Color[] palette = new Color[256];
 
-        for (int i = 0; i < palette.length; i++) {
-            int shade = i / 4;
-            palette[i] = new Color(shade,shade,shade);
+        for (int i = 0; i < 256; i++) {
+            palette[i] = new Color(255-i,255-i,255-i);
         }
 
         return palette;
     }
+
 }
